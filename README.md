7f8de459f5f4df4554efcd16ef101e7c2551187b
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-06-05 23:04:47 -0500

    Add progress indicator

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
-# Node.js simple file upload
-
-### Dependencies
-- express
-- ejs
-- fs
-- multer
-
+# Node.js simple multi-file upload with progress indicator
 
 ### Run
 ```
-> git clone https://github.com/Majidkn/nodejs-simple-file-upload.git
+> git clone https://github.com/abhinavpappu/nodejs-simple-file-upload.git
 > cd nodejs-simple-file-upload
 > npm install
 > npm start
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
-var multer  = require('multer');
-var fs  = require('fs');
+var multer = require('multer');
+var fs = require('fs');
+const progress = require('progress-stream');
+const terminalOverwrite = require('terminal-overwrite');
+const prettyBytes = require('pretty-bytes');
+const humanizeDuration = require('humanize-duration');
+
+const UPLOAD_DIR = './uploads';
+
+let startTime = Date.now();
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -11,8 +19,8 @@ app.get('/', (req, res) => {
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        var dir = './uploads';
-        if (!fs.existsSync(dir)){
+        var dir = UPLOAD_DIR;
+        if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
         }
         callback(null, dir);
@@ -21,14 +29,33 @@ var storage = multer.diskStorage({
         callback(null, file.originalname);
     }
 });
-var upload = multer({storage: storage}).array('files', 12);
+var upload = multer({ storage: storage }).array('files');
 app.post('/upload', function (req, res, next) {
-    upload(req, res, function (err) {
+    const requestSize = Number(req.get('content-length'));
+    const p = progress({ time: 1000, length: requestSize });
+    console.log(`New request with size: ${prettyBytes(requestSize)}\n`);
+    req.pipe(p);
+    p.headers = req.headers;
+    startTime = Date.now();
+
+    p.on('progress', prog => {
+        terminalOverwrite(`${prog.percentage}% complete\n${prettyBytes(prog.transferred)} transferred\n ${prettyBytes(prog.speed)} per second\n${humanizeDuration(Date.now() - startTime)}\n${prettyBytes(prog.length)} total\n${prettyBytes(prog.remaining)} remaining`);
+    });
+
+    upload(p, res, function (err) {
         if (err) {
-            return res.end("Something went wrong:(");
+            return res.end("Something went wrong :(");
         }
         res.end("Upload completed.");
+        terminalOverwrite.done();
+        console.log();
     });
+});
+
+app.use(function (err, req, res, next) {
+    console.log(err.status);
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 })
 
-app.listen(3000);
+app.listen(3000, () => console.log('Listening on port 3000...\n'));
